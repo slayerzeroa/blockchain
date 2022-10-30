f0d1211c9e6d12a6978a8df76db0db090e6b869c
Remix Workspace <>
2022-10-30 12:28:23 +0900

    YDM_221030

---
+++ pompay_token.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier:GPL-30
+pragma solidity >= 0.7.0 < 0.9.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+
+contract PompayToken is ERC20{
+    constructor(string memory name, string memory symbol) ERC20(name,symbol){
+        _mint(msg.sender, 1000*10**uint(decimals()));
+    }
+}
