a36a612b3959656c38d9eaccfec2e14430c2a83a
Remix Workspace <>
2022-10-30 21:24:30 +0900

    YDM_221030_1

--- pompay_token.sol
+++ pompay_token.sol
@@ -4,7 +4,9 @@ pragma solidity >= 0.7.0 < 0.9.0;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 
 contract PompayToken is ERC20{
+    uint public INITIAL_SUPPLY = 150*10**18;
+    
     constructor(string memory name, string memory symbol) ERC20(name,symbol){
-        _mint(msg.sender, 1000*10**uint(decimals()));
+        _mint(msg.sender, INITIAL_SUPPLY);
     }
 }
