71c2d153fc12b9fd51619043465f707e06bfc192
slayerzeroa <slayerzeroa@gmail.com>
2022-11-20 22:28:42 +0900

    YDM_221120

--- sol/pompay_token.sol
+++ sol/pompay_token.sol
@@ -4,8 +4,7 @@ pragma solidity >= 0.7.0 < 0.9.0;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 
 contract PompayToken is ERC20{
-    uint public INITIAL_SUPPLY = 150*10**18;
-    
+    uint public INITIAL_SUPPLY = 1500000000000000*10**18;
     constructor(string memory name, string memory symbol) ERC20(name,symbol){
         _mint(msg.sender, INITIAL_SUPPLY);
     }
