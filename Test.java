2d03ebe5d9092a0872cf0eab026a7cdf14716615
jihyun0822 <105538788+jihyun0822@users.noreply.github.com>
2022-11-23 21:41:32 +0900

    Update Test.java
    eclipse랑 mysql연동할 때 eclipse 안에 있는 pom.xml파일에 
    <dependency>   
    
    		  	<groupId>mysql</groupId>   
    		
    		    	<artifactId>mysql-connector-java</artifactId>
    		
    		    	<version>5.1.45</version>
    
        </dependency>
    이 코드 추가해야해요

--- Test.java
+++ Test.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Test {
-
+//재코밋
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		  try {
