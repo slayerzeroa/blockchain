9ee34c106555840d0af2543995a9908466c8577b
slayerzeroa <slayerzeroa@gmail.com>
2022-11-27 18:26:41 +0900

    ydm_221127

--- metamask/src/App.js
+++ metamask/src/App.js
@@ -1,12 +1,39 @@
 import { useWeb3React } from "@web3-react/core";
 import { injected } from "./lib/connectors";
 import { isNoEthereumObject } from "./lib/errors";
+// import { useEffect, useState } from "react";
 
 import "./styles.css";
+
+
+const Web3 = require('web3');
+const provider = 'https://sepolia.infura.io/v3/9041415e6a4c43d9a9475c8d19a57d0b';
+const web3 = new Web3(new Web3.providers.HttpProvider(provider));
+const token = '0x81A0918aAb818cDaD7dA31A8cD99eF1D0BD2CDbf';
+const wallet = '0x98c76c474b5748dbe024de0deb5d098b5c648c07';
+const minABI = [
+  {
+      constant: true,
+      inputs: [{ name: "_owner", type: "address" }],
+      name: "balanceOf",
+      outputs: [{ name: "balance", type: "uint256" }],
+      type: "function",
+  },
+];
+
+const contract = new web3.eth.Contract(minABI, token);
+
 
 export default function App() {
   const { chainId, account, active, activate, deactivate } = useWeb3React();
 
+  const getBalance = async () => {
+    const res = await contract.methods.balanceOf(wallet).call();
+    const format = web3.utils.fromWei(res);
+    console.log(format);
+    return format;
+  }
+  
   const handleConnect = () => {
     if (active) {
       deactivate();
@@ -23,6 +50,7 @@ export default function App() {
       <div className="user">
         <p>Account: {account}</p>
         <p>ChainId: {chainId}</p>
+        <p>Balance: </p>
       </div>
       <div className="connect">
         <button type="button" onClick={handleConnect}>
