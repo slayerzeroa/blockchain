13e87b7068ad17ecb8dd877892d4808a38fa5227
Psyduck_20 <66348567+slayerzeroa@users.noreply.github.com>
2023-01-21 22:04:46 +0900

    Delete src/main/pompay_paran directory

--- src/main/pompay_paran/client/src/_reducers/index.js
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from 'redux';
-import user from './user_reducer';
-//import comment from './comment_reducer';
-
-const rootReducer = combineReducers({
-    user,
-})
-
-export default rootReducer;
